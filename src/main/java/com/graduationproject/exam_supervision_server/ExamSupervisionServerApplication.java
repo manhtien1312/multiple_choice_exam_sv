431edd67e61821ada3675dea14a28f06d06b927d
Nguyễn Mạnh Tiến <tiennguyenmanh131202@gmail.com>
2024-10-07 15:29:29 +0700

    first commit

---
+++ src/main/java/com/graduationproject/exam_supervision_server/ExamSupervisionServerApplication.java
@@ -0,0 +1,13 @@
+package com.graduationproject.exam_supervision_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSupervisionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSupervisionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graduationproject/exam_supervision_server/ExamSupervisionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduationproject.exam_supervision_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSupervisionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
