0f995b3540e64068dc7f20cbee94590ae159ff22
VQuangDung <vuquangdungptit1404@gmail.com>
2024-10-19 20:03:05 +0700

    Add files via upload

---
+++ src/model/ExamResult.java
@@ -0,0 +1,40 @@
+package com.graduationproject.exam_supervision_server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "exam_result")
+public class ExamResult {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    private Integer correctQuestions;
+    private Integer wrongQuestions;
+    private Integer blankQuestions;
+
+    @ManyToMany(mappedBy = "examResults")
+    private List<Report> reports;
+
+}
